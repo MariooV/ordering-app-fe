7b8ce70c21a7a361fb4a099a486246db26c443cb
Mario Vrevic <mariovrevic@gmail.com>
2022-06-20 15:10:07 +0200

    .

--- src/Pages/Landing/Landing.tsx
+++ src/Pages/Landing/Landing.tsx
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Landing = () => {
-    return <>Landing Page</>
-}
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/login");
+  }, []);
+
+  return <>Landing Page</>;
+};
 
 export default Landing;
